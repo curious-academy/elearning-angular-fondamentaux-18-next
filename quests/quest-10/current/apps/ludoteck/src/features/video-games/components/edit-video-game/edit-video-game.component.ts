0891cbe04667e5614492f16096a86fa2a29577d2
Evan BOISSONNOT <evan@boissonnot.fr>
2025-07-14 18:45:32 +0200

    feat: fin quete 011

--- quests/quest-10/current/apps/ludoteck/src/features/video-games/components/edit-video-game/edit-video-game.component.ts
+++ quests/quest-10/current/apps/ludoteck/src/features/video-games/components/edit-video-game/edit-video-game.component.ts
@@ -1,21 +1,57 @@
-import { Component, computed, inject, OnInit } from '@angular/core';
-import { toSignal } from '@angular/core/rxjs-interop';
+import { Component, computed, inject, OnInit, signal } from '@angular/core';
+import { takeUntilDestroyed, toSignal } from '@angular/core/rxjs-interop';
+import { FormBuilder, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { UpdateOneVideoGameService } from '../../services/update-one-video-game.service';
+import { VideoGame } from '../../models/video-game';
+import { GetOneVideoGameService } from '../../services/get-one-video-game.service';
+import { concatMap, distinctUntilChanged, map, pairwise, switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'app-edit-video-game',
-  imports: [],
+  imports: [ReactiveFormsModule, MatButtonModule, MatFormFieldModule, MatInputModule, MatIconModule],
   templateUrl: './edit-video-game.component.html',
   styleUrl: './edit-video-game.component.css'
 })
 export class EditVideoGameComponent implements OnInit {
+  private readonly formBuilder = inject(FormBuilder)
+  private readonly getOneService = inject(GetOneVideoGameService)
+  private readonly updateOneService = inject(UpdateOneVideoGameService)
+
+  protected readonly isLoading = signal(false)
+
+  protected readonly videoGameFormGroup = this.formBuilder.nonNullable.group({
+    label: ['', [Validators.required, Validators.minLength(3)]],
+    year: [new Date().getFullYear(), [Validators.required, Validators.max(new Date().getFullYear())]],
+  })
+
   private readonly route = inject(ActivatedRoute)
+
+  protected readonly setVideoGame$ = this.route.params
+    .pipe(
+      tap(() => this.isLoading.set(true)),
+      map<{ [key: string | symbol]: any }, number>(params => params['id']),
+      switchMap(id => this.getOneService.getOne(id)),
+      tap(() => this.isLoading.set(false)),
+    )
+
+  protected readonly detectChanges$ = this.videoGameFormGroup.valueChanges
+    .pipe(
+      pairwise(),
+      distinctUntilChanged(),
+      takeUntilDestroyed()
+    )
+
   videoGameParams = toSignal(this.route.params)
   videoGameId = computed(() => {
     let id = 0
     const params = this.videoGameParams()
 
-    if(params) {
+    if (params) {
       id = params['id']
     }
 
@@ -25,11 +61,33 @@ export class EditVideoGameComponent implements OnInit {
   ngOnInit(): void {
     console.info(this.route.snapshot.params['id'])
 
-    this.route.params.subscribe({
-      next: item => {
-        console.info(item['id'])
-      }
+    this.setVideoGame$
+      .subscribe({
+        next: item => {
+          this.videoGameFormGroup.patchValue(item)
+        }
+      })
+
+
+    this.detectChanges$
+    .subscribe({
+      next: change => console.info(change)
     })
   }
 
+  saveOne(): void {
+    const videoGame: VideoGame = {
+      label: '',
+      year: 0
+    };
+
+    if (this.videoGameFormGroup.value.label) {
+      videoGame.label = this.videoGameFormGroup.value.label;
+    }
+    if (this.videoGameFormGroup.value.year) {
+      videoGame.year = this.videoGameFormGroup.value.year;
+    }
+
+    this.updateOneService.updateOne(videoGame).subscribe()
+  }
 }
--- quests/quest-10/current/apps/ludoteck/src/features/video-games/components/table-video-games/table-video-games.component.ts
+++ quests/quest-10/current/apps/ludoteck/src/features/video-games/components/table-video-games/table-video-games.component.ts
@@ -1,5 +1,6 @@
-import { Component, input, output } from '@angular/core';
+import { Component, inject, input, output } from '@angular/core';
 import { VideoGame, VideoGames } from '../../models/video-game';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-table-video-games',
@@ -9,6 +10,12 @@ import { VideoGame, VideoGames } from '../../models/video-game';
   styleUrl: './table-video-games.component.css'
 })
 export class TableVideoGamesComponent {
+  private readonly router = inject(Router)
+
+  editOne(item: VideoGame): void {
+    this.router.navigate(['jeux-videos', item.id])
+  }
+
   items = input.required<VideoGames>();
   title = input.required<string>();
 
