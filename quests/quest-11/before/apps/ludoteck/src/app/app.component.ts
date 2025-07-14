5ef6ad3b6753e2c5e5ad66f24485fe8ba16916b3
Evan BOISSONNOT <evan@boissonnot.fr>
2025-07-14 16:02:43 +0200

    chore: prepare 11

---
+++ quests/quest-11/before/apps/ludoteck/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { MatSliderModule } from '@angular/material/slider';
+import { RouterOutlet } from '@angular/router';
+import { MainMenuComponent } from '../shared/menus/main-menu/main-menu.component';
+@Component({
+    imports: [RouterOutlet, MatSliderModule, MainMenuComponent],
+    selector: 'app-root',
+    // template: `
+    // <div class="coucou">
+    //     <app-list-video-games></app-list-video-games>
+    // </div>
+    // `,
+    templateUrl: './app.component.html',
+    //styles: ['div.coucou {}'],
+    styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'ludoteck';
+}
---
+++ quests/quest-11/current/apps/ludoteck/src/features/video-games/components/table-video-games/table-video-games.component.ts
@@ -0,0 +1,25 @@
+import { Component, input, output } from '@angular/core';
+import { VideoGame, VideoGames } from '../../models/video-game';
+
+@Component({
+  selector: 'app-table-video-games',
+  standalone: true,
+  imports: [],
+  templateUrl: './table-video-games.component.html',
+  styleUrl: './table-video-games.component.css'
+})
+export class TableVideoGamesComponent {
+  items = input.required<VideoGames>();
+  title = input.required<string>();
+
+  askToRemoveOneElement = output<string>();
+  askToRemoveOneSpecific = output<VideoGame>();
+
+  clickToRemove(): void {
+    this.askToRemoveOneElement.emit('test');
+  }
+
+  clickToRemoveSpecificElement(item: VideoGame): void {
+    this.askToRemoveOneSpecific.emit(item);
+  }
+}
